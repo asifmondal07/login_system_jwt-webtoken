b46facdff7d1d9bf1765d3a1840c49ce387f43ea
asifmonda07 <asikbal7ai@gmail.com>
2025-02-16 12:31:02 +0530

    alll done

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,6 +1,7 @@
 const { default: mongoose } = require("mongoose");
 const Blog = require("../models/blog");
-const Comment=require("../models/comment")
+const Comment=require("../models/comment");
+const { findById } = require("../models/user");
 
 async function createBlog(req,res){
     
@@ -28,33 +29,54 @@ async function createBlog(req,res){
 
 async function getBlogs(req,res){
     try {
-        const blog=await Blog.find().populate("author","name email");
-        if(!blog.length) {
-            return res.status(404).json({ message: "No blogs found" });
+        let {page, limit,short}=req.params;
+
+        page=parseInt(page)||1
+        limit=parseInt(limit)||5
+        const skip=(page-1)*limit;
+
+        let shortoption={createdAt :-1};
+        if(short === "oldest"){
+            shortoption={createdAt: 1};
         }
 
+        const blogs=await Blog.find()
+        .sort(shortoption)
+        .skip(skip).limit(limit);
 
-        const blogsWithComments= await Promise.all(
-            blog.map(async (blog)=>{
-                const comments=await Comment.find({blogId:blog._id})
-                .populate("createdBy","name")
-                .sort({createdAt:-1});
+        const totalblogs=await Blog.countDocuments();
 
-                const blogogject=blog.toObject();
-                blogogject.comments=comments;
+        res.status(201)
+        .json({page:page,
+            limit:limit,
+            totalblogs:totalblogs,
+            totalpages:Math.ceil(totalblogs/limit),
+            blogs:blogs})
 
-                return blogogject
-            })
+
+    
+        // const blogsWithComments= await Promise.all(
+        //     blog.map(async (blog)=>{
+        //         const comments=await Comment.find({blogId:blog._id})
+        //         .populate("createdBy","name")
+        //         .sort({createdAt:-1});
+
+        //         const blogogject=blog.toObject();
+        //         blogogject.comments=comments;
+
+        //         return blogogject
+        //     })
             
-        )
+        // )
         
-        
-
-        return res.status(201).json({Blog:blogsWithComments});
         
         
     } catch (error) {
-        return res.status(200).json({message:"Error Fetching blog",error})
+        console.error("Error fetching blogs:", error); // Log full error details
+        return res.status(500).json({ 
+            message: "Error Fetching blog", 
+            error: error.message || error 
+        });
     }
 }
 
@@ -83,5 +105,40 @@ async function deleteBlogs(req,res){
     
 
 }
+async function handelEditBlog(req,res){
+    try {
+        const {blogId}=req.params;
+        const userId=req.user._id;
+        const{title ,content}=req.body;
 
-module.exports={createBlog,getBlogs,deleteBlogs};
+        const blog=await Blog.findById(blogId);
+        if(!blog){return res.status(404).json({message:"Blog Not Found"})};
+
+        if(blog.author.toString !== userId.toString){return res.status(403)
+            .json({message: "Unauthorized! You can only delete your own blog."})}
+
+        // Update the blog
+        blog.title=title||blog.title; // If no new title, keep old one
+        blog.content=content||blog.content; // If no new content, keep old one
+        await blog.save() //save the udit blog
+        res.status(200).json({message:"Blog Editing succsesfull ",title:blog.title ,content:blog.content});
+        
+
+    } catch (error) {
+        res.status(500).json({message:"Error Editing Blog",error:error.message});
+    }
+    
+}
+
+async function handelGetSingleBlog(req,res){
+    try {
+        const {blogId}=req.params;
+        const blog=await Blog.findById(blogId).populate("author","name email");
+        if(!blog){return res.status(500).json({message:"Blog Not Found"})}
+        res.status(200).json({blog:blog});
+    } catch (error) {
+        res.status(500).json({message:"Error Get Blog",error:error.message});
+    }
+}
+
+module.exports={createBlog,getBlogs,deleteBlogs,handelEditBlog,handelGetSingleBlog};
--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,6 @@
 const express=require("express");
 
-const {createBlog ,getBlogs,deleteBlogs}= require("../controllers/blog.js");
+const {createBlog ,getBlogs,deleteBlogs,handelEditBlog,handelGetSingleBlog}= require("../controllers/blog.js");
 const requiredAuth = require("../middileware/author");
 
 const router=express.Router();
@@ -8,6 +8,8 @@ const router=express.Router();
 router.post("/create",requiredAuth, createBlog);
 router.get("/",getBlogs);
 router.delete("/:blogId",requiredAuth, deleteBlogs);
+router.patch("/:blogId",requiredAuth,handelEditBlog);
+router.get("/:blogId",handelGetSingleBlog);
 
 
 module.exports=router;
